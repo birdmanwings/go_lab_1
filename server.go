41667567f04721b325c67496e737d32d6bfa5853
birdmanwings <747458025@qq.com>
2019-05-22 23:38:51 +0800

    Initial commit

---
+++ server.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"strings"
+)
+
+func connHandler(c net.Conn) {
+	if c == nil {
+		return
+	}
+
+	buf := make([]byte, 4096)
+
+	for {
+		//读取并存入buf中
+		cnt, err := c.Read(buf)
+
+		if err != nil || cnt == 0 {
+			c.Close()
+			break
+		}
+
+		inStr := strings.TrimSpace(string(buf[0:cnt]))
+
+		inputs := strings.Split(inStr, " ")
+
+		//根据客户端的选项
+		switch inputs[0] {
+		case "ping":
+			c.Write([]byte("pong\n"))
+		case "echo":
+			echoStr := strings.Join(inputs[1:], " ") + "\n"
+			c.Write([]byte(echoStr))
+		case "quit":
+			c.Close()
+			break
+		default:
+			fmt.Printf("Unsupported command: %s\n", inputs[0])
+		}
+	}
+
+	fmt.Printf("Connection from %v closed. \n", c.RemoteAddr())
+}
+
+func main() {
+	server, err := net.Listen("tcp", ":1208")
+	if err != nil {
+		fmt.Printf("Fail to start server,%s\n", err)
+	}
+	fmt.Println("Server Started")
+
+	for {
+		conn, err := server.Accept()
+		if err != nil {
+			fmt.Printf("Fail to connect, %s \n", err)
+		}
+
+		//服务器端使用协程
+		go connHandler(conn)
+	}
+}
